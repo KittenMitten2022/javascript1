d4d269de1538e7e8f5bfa33699127109957c57d7
KittenMitten2022 <shaynanakamura@student.midpac.edu>
2015-09-16 16:31:30 -1000

    Javascript1

---
+++ app.js
@@ -0,0 +1,8 @@
+var press = function() {
+	alert("Hello! the button you just pressed was totally worthless exept for this message. Troll. =) you are probally mad now. Haa Haa Haa!!! LOL!!!");
+	} 
+
+var ask = function() {
+	var name = prompt ("What is ur name?"); 
+	alert("Hello " + name + "SUNSHINE AND RAINBOWS ARE AWESOME!!!!!!!!!! " );
+}
